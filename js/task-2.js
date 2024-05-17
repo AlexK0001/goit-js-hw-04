c82e04582f39cba4c4604254de95ddce763f9e00
Alexandr <alexandr.kopyletz@gmail.com>
2024-05-17 14:14:24 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,13 @@
 function calcAverageCalories(days) {
-    let totalCalories = 0;
-    for (const day of Object.values(days)) {
-        totalCalories += day.calories;
+  let totalCalories = 0;
+  if (days.length !== 0) {
+    for (const day of days) {
+      totalCalories += day.calories;
     }
-    return totalCalories / 7;
+    return totalCalories / days.length;
+  } else {
+    return 0;
+  }
 }
 console.log(
     calcAverageCalories([
